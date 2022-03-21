4214c75a49d7d298ec616b3a3d059faf735fd671
Orinda Felix Ochieng <forinda82@gmail.com>
2022-03-21 17:47:22 +0300

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,26 @@
-import React, { useRef, useState } from "react";
+import { useRef, useState } from "react";
 import "./App.css";
 
 function App() {
   const [open, setOpen] = useState<boolean>(false);
-  const dropDownRef = useRef<HTMLDivElement>(null);
-
-  const handleClickOutsideDropDown = (e: any) => {
-    if (open && !dropDownRef.current?.contains(e.target as Node)) {
-      setOpen(false);
+  const dropdownRef = useRef<HTMLDivElement>(null)
+  const handleDropDownFocus = (state: boolean) => {
+    setOpen(!state);
+  };
+  const handleClickOutsideDropdown =(e:any)=>{
+    if(open && !dropdownRef.current?.contains(e.target as Node)){
+      setOpen(false)
+      
     }
-  };
-  window.addEventListener("click", handleClickOutsideDropDown);
-  const handleFocus = () => {
-    setOpen(!open);
-  };
+  }
+  window.addEventListener("click",handleClickOutsideDropdown)
+  
   console.log(open);
 
   return (
     <div className="App">
-      <div className="app-drop-down-container" ref={dropDownRef}>
-        <button onClick={handleFocus}>Drop down</button>
+      <div className="app-drop-down-container" ref={dropdownRef}>
+        <button onClick={(e) => handleDropDownFocus(open)}>Drop down</button>
         {open && (
           <ul>
             <li>Item 1</li>
